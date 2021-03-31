776c50b8df12dda4262b794b79efb412e337e320
Santiago Vergara <santiagovrlr@gmail.com>
2021-03-31 14:06:46 -0600

    Primer Mockup

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,11 +11,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Wall Breaker");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
-    
+
     public static void main(String[] args) {
         launch(args);
     }
