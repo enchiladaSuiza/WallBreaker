202e2800f9a1f4593a27c6aecc7790ce2a10fe23
Santiago Vergara <santiagovrlr@gmail.com>
2021-04-10 13:40:02 -0500

    Productos Select

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,47 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private AnchorPane centro, arriba;
+    @FXML
+    private ScrollPane izquierda;
+    @FXML
+    private Button productosBtn, ventasBtn, proveedoresBtn, analisisBtn, pedidosBtn, personalBtn;
+    @FXML
+    private Label titulo;
+    @FXML
+    private TextArea select;
+
+    private Datos datos;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        datos = Main.conseguirDatos(); // Puede no ser el mejor acercamiento
+    }
+
+    public void pantallaProductos() {
+        titulo.setText("Productos");
+        ArrayList<String> tuplas;
+        try {
+            tuplas = datos.verTodo("producto");
+        } catch (SQLException throwables) {
+            select.setText("Error!");
+            return;
+        }
+        StringBuilder consulta = new StringBuilder();
+        for (String tupla : tuplas) {
+            consulta.append(tupla).append("\n");
+        }
+        select.setText(consulta.toString());
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,23 +4,58 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextInputDialog;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
+    private static Datos datos;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Principal.fxml"));
+        TextInputDialog inputUsuario = new TextInputDialog("root");
+        inputUsuario.setContentText("Nombre de usuario de MySQL para acceder a la base de datos");
+        inputUsuario.setTitle("Wall Breaker");
+        inputUsuario.setHeaderText("Usuario de MySQL");
+        inputUsuario.showAndWait();
+        String usuario = inputUsuario.getEditor().getText();
+
+        TextInputDialog inputPassword = new TextInputDialog();
+        inputPassword.setContentText("Contraseña del usuario");
+        inputPassword.setTitle("Wall Breaker");
+        inputPassword.setHeaderText("Contraseña de usuario");
+        inputPassword.showAndWait();
+        String password = inputPassword.getEditor().getText();
+
+        try {
+            datos = new Datos(usuario, password);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            Alert error = new Alert(Alert.AlertType.ERROR);
+            error.setTitle("Wall Breaker");
+            error.setHeaderText("Error conectando a la base de datos");
+            error.setContentText("Puede que el usuario y/o contraseña sean incorrectos, o que el servidor no esté " +
+                    "corrinedo");
+            error.showAndWait();
+            return;
+        }
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Principal.fxml")));
         primaryStage.setTitle("Wall Breaker");
         Scene scene = new Scene(root);
-        String css = getClass().getResource("style.css").toExternalForm();
+        String css = Objects.requireNonNull(getClass().getResource("style.css")).toExternalForm();
         scene.getStylesheets().add(css);
-
         primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public static Datos conseguirDatos() {
+        return datos;
     }
 }
